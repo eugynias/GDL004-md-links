f153ec6c2e4ac2484513dc77d95e897c39f0dc68
unknown <eugynias>
2020-02-24 09:24:18 -0500

    Funcionando los 4 archivos js para filtro de links y prueba de revision de links

--- src/options.js
+++ src/options.js
@@ -7,17 +7,7 @@ arrayreadFile(pathfs,(error,data)=>{
     if (error){
     console.log(error)
     }
-      for (let i=0 ;i<data.length;i++){
-          ping (data[i])
-      }
-      function ping (url){
-          const start= new Date()
-
-          http.get({host:url}, function (res){
-              console.log ("URL:" + url)
-              console.log ("Response Time:" +(new Date()-start)+"ms")
-          })
-      }
+      console.log(data)
 
 })
 
